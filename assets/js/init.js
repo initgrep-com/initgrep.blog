ce48004135239afdd03c535bd2e2e4c8c7dfe8b5
Irshad Ahmad Sheikh <irshsheikh@gmail.com>
2016-12-16 01:13:54 +0530

    stylebar edit

--- assets/js/init.js
+++ assets/js/init.js
@@ -3,11 +3,15 @@
 
   	$(".dropdown-button").dropdown();
   	$(".dropdown-content").css("width","300px");
-    $('.button-collapse').sideNav();
+     //$('span.datetime').html(timeSince($("span.datetime").html()));
 
-    //$('span.datetime').html(timeSince($("span.datetime").html()));
-
-    
+      $('.button-collapse').sideNav({
+          menuWidth: 300, // Default is 240
+          edge: 'left', // Choose the horizontal origin
+          closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+          draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
 
 	 
   }); // end of document ready
