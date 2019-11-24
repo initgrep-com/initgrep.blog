111da24cc1d5f6e591b967bfd37c52490b298371
igagrock <irshad.mike@gmail.com>
2019-11-24 23:05:15 +0400

    added onhover and onscroll hide events for menu component

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,66 @@
 class InitGrep {
+    hasMenuOpacityEnabled = false;
+    hoveredIn = false;
 
-    enableScrollProgress() {
-        
+    updateScrollChangeEvents() {
+
         document.addEventListener(
             "scroll",
-            function () {
-                console.log("oncroll called");
+            () => {
+                console.log("onscroll called");
                 const scrollTop =
                     document.documentElement["scrollTop"] || document.body["scrollTop"];
                 const scrollBottom =
                     (document.documentElement["scrollHeight"] ||
                         document.body["scrollHeight"]) - document.documentElement.clientHeight;
-               const scrollPercent = scrollTop / scrollBottom * 100 + "%";
-                document
-                    .getElementById("progress")
-                    .style.setProperty("--scroll", scrollPercent);
+                const scrollPercent = scrollTop / scrollBottom * 100;
+                const scrollPercentString = scrollTop / scrollBottom * 100 + "%";
+
+                //    console.log("scrolltop ",scrollTop);
+                //    console.log("scrollBottom ",scrollBottom, scrollPercent);
+
+                this.updateProgressScrollBar(scrollPercentString);
+                this.changeMenuOpacityOnScroll(scrollTop);
             },
             { passive: true }
+        );
+    }
+
+    updateProgressScrollBar(scrollPercent) {
+        document
+            .getElementById("progress")
+            .style.setProperty("--scroll", scrollPercent);
+    }
+
+    changeMenuOpacityOnScroll(scrollPercent) {
+        if (scrollPercent >= 3000 && !this.hasMenuOpacityEnabled) {
+            console.log("add the opacity");
+            this.hasMenuOpacityEnabled = true;
+            $('.menu-component').addClass('scroll-opactity');
+        }
+        if (scrollPercent < 3000 && this.hasMenuOpacityEnabled) {
+            this.hasMenuOpacityEnabled = false;
+            console.log("remove the opacity");
+            $('.menu-component').removeClass('scroll-opactity');
+        }
+    }
+
+    changeMenuOpactiyOnHover() {
+        $('.menu-component').hover(
+            () => {
+                if (this.hasMenuOpacityEnabled) {
+                    $('.menu-component').removeClass('scroll-opactity');
+                    this.hasMenuOpacityEnabled = false;
+                    this.hoveredIn = true;
+                }
+            },
+            () => {
+                if (!this.hasMenuOpacityEnabled && this.hoveredIn) {
+                    $('.menu-component').addClass('scroll-opactity');
+                    this.hasMenuOpacityEnabled = true;
+                    this.hoveredIn = false;
+                }
+            }
         );
     }
 
@@ -66,7 +110,7 @@ class InitGrep {
 //run scripts below
 
 const initgrep = new InitGrep();
-initgrep.enableScrollProgress();
+initgrep.updateScrollChangeEvents();
 initgrep.hideBasicOverlay();
 initgrep.hideSearchWrapper();
 initgrep.hideSettingWrapper();
@@ -74,3 +118,4 @@ initgrep.closeSearchWrapper();
 initgrep.openSearchWrapper();
 initgrep.openSettingWrapper();
 initgrep.closeSettingWrapper();
+initgrep.changeMenuOpactiyOnHover();
